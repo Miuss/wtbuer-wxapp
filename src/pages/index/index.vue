ccfdcf5d51833e7c88b9d7b46c05eb7dbc4bc3b3
Jamartin <martin_du@qq.com>
2022-01-16 19:23:49 +0800

    feat:底部导航栏以及对应组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,126 +1,53 @@
 <template>
-  <div @click="clickHandle">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
+  <div>
+    <div class="container">
+      <home v-if="active==0"/>
+      <schedule v-if="active==1"/>
+      <square v-if="active==2"/>
+      <community v-if="active==3"/>
+      <mine v-if="active==4"/>
     </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
+    <van-tabbar :active="active" @change="onChange">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="notes-o">课表</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">广场</van-tabbar-item>
+      <van-tabbar-item icon="volume-o">社区</van-tabbar-item>
+      <van-tabbar-item icon="contact">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
+import home from './plate/home/'
+import mine from './plate/mine/'
+import community from './plate/community/'
+import schedule from './plate/schedule/'
+import square from './plate/square/'
 
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      active: 0
     }
   },
-
-  components: {
-    card
-  },
-
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
+    onChange (event) {
+      this.active = event.mp.detail
     }
   },
-
+  components: {
+    home,
+    mine,
+    community,
+    schedule,
+    square
+  },
   created () {
-    // let app = getApp()
+
   }
 }
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
 </style>
